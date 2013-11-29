cca8a9c1f3563885e3609a8ac75ba35f849a8cb2
Romain Gauthier <rj.gauthier@laposte.net>
2013-11-29 07:22:46 +0100

    game of life: add license to graph.c/.h

--- graph.c
+++ graph.c
@@ -1,1 +1,18 @@
+/*
+    The following program implements a command-line game of life, originally
+    introduced by John Conway in 1970.
+
+    This program is licensed under a GNU GPL 3.0 License.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+
+    You should have received a copy of the GNU GPL 3.0 License along with this
+    program. If not, see <http://www.gnu.org/licenses/gpl.html>.
+
+    Copyright (c) 2013 Romain Gauthier <rj.gauthier@laposte.net>
+*/
+
+
 #include "graph.h"
